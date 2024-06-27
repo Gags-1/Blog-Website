c0b5ed22a5e881492b7fb22d7cf64d20b65b327a
Gags-1 <104347087+Gags-1@users.noreply.github.com>
2024-06-27 16:16:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,12 +6,10 @@ app= FastAPI()
 
 # models.Base.metadata.create_all(bind=engine)
 
-origins=[
-   "http://localhost:3000"
-]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
